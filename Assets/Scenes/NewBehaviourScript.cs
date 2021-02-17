39f9da4f55829058fabea7ba68658f9b42394b4f
Eesan <mister.eisen@gmail.com>
2021-02-16 21:58:34 -0500

    Ball Kicking Sim Done

---
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space")) //when space is pressed it activates the kick
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(0, 300.0f, 1000.0f); //gives a force that simulates a kick
+        }
+
+        if (Input.GetKeyDown("return")) //reset to original position and velocity
+        {
+            gameObject.transform.position = new Vector3(-0.17f, 1.6f, -6.7f);
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 EisenEnrique_GAME3002_A#
+
+Controls:
+Space - Launch the ball
+Enter - Reset the ball
+
